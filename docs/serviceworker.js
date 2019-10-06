51d2725274295294210bc04047e509c5286a5c2f
rburkitt <rburkitt@users.noreply.github.com>
2019-10-06 19:40:11 -0400

    caching files offline

--- docs/serviceworker.js
+++ docs/serviceworker.js
@@ -1,1 +1,13 @@
-
+var CACHE_NAME = 'static-cache';
+var urlsToCache = [
+  '.',
+  'index.html'
+];
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
